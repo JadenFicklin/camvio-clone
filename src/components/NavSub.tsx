6bc1bff1865e16f60f29d3a0ffd22e04d95de441
JadenFicklin <jadendficklin@gmail.com>
2024-04-14 20:45:59 -0600

    apis

--- src/components/NavSub.tsx
+++ src/components/NavSub.tsx
@@ -1,17 +1,28 @@
+import { Link } from 'react-router-dom'
+
 export const NavSub = () => {
   return (
     <>
       <nav className="w-full py-3 h-max bg-max">
         <div className="flex flex-wrap justify-center mx-auto space-x-5 sm:w-10/12 xl:w-8/12 lg:justify-start text-min">
-          <div className="text-white duration-150 cursor-pointer hover:text-gray-300">
+          <Link
+            to="/announcements"
+            className="text-white duration-150 cursor-pointer hover:text-gray-300"
+          >
             Announcements
-          </div>
-          <div className="text-white duration-150 cursor-pointer hover:text-gray-300">
+          </Link>
+          <Link
+            to="/"
+            className="text-white duration-150 cursor-pointer hover:text-gray-300"
+          >
             Blog
-          </div>
-          <div className="text-white duration-150 cursor-pointer hover:text-gray-300">
+          </Link>
+          <Link
+            to="/"
+            className="text-white duration-150 cursor-pointer hover:text-gray-300"
+          >
             Support
-          </div>
+          </Link>
         </div>
       </nav>
     </>
--- src/pages/Announcements.tsx
+++ src/pages/Announcements.tsx
@@ -1,7 +1,72 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { NavSub } from '~/components/NavSub'
+
+interface Pokemon {
+  name: string
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string
+      }
+    }
+  }
+  abilities: Ability[]
+}
+
+interface Ability {
+  ability: {
+    name: string
+    url: string
+  }
+  is_hidden: boolean
+  slot: number
+}
+
 export const Announcements = () => {
+  const [data, setData] = useState<Pokemon | null>(null)
+  const [name, setName] = useState<string | undefined>()
+  const [number, setNumber] = useState(1)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const URL = `https://pokeapi.co/api/v2/pokemon/${number}`
+      try {
+        const response = await axios.get<Pokemon>(URL)
+        setData(response.data)
+        setName(response.data.name)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    fetchData()
+  }, [number])
+
   return (
     <>
-      <div>Announcements</div>
+      <div>
+        <NavSub />
+        <div>{number}</div>
+        Announcements
+        <button className="bg-blue-500" onClick={() => setNumber(number + 1)}>
+          add 1
+        </button>
+        <button className="bg-red-500" onClick={() => setNumber(number - 1)}>
+          subtract 1
+        </button>
+        <h2 className="text-4xl font-bold text-red-500">{name}</h2>
+        {data && data.sprites.other.dream_world.front_default && (
+          <img
+            src={data.sprites.other.dream_world.front_default}
+            alt={`Image of ${name}`}
+          />
+        )}
+        {data &&
+          data.abilities.map((ability, key) => (
+            <div key={key}>{ability.ability.name}</div>
+          ))}
+      </div>
     </>
   )
 }
